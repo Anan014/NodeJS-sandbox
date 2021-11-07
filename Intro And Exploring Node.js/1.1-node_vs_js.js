fb0541e5866278a45d127a36b78060239b9b0028
Anan014 <ananamer95@gmail.com>
2021-11-07 11:49:17 +0200

    first commit

---
+++ Intro And Exploring Node.js/1.1-node_vs_js.js
@@ -0,0 +1,21 @@
+/*
+Write in your own words the following:
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+1) What are 3 things node.js has that the client side javascript doesn’t
+have. And the reasons why.
+2)What are 3 things the client side javascript has that node.js doesn’t
+have. And the reasons why.
+
+1-type:
+-JavaScript: JavaScript is a programming language. It is running in any web browser with a proper browser engine.
+-NodeJS: It is a interpeter and enviromnet for JavaScript with some specific useful librareis with JavaScript programming can use separately.
+
+2-Utility:
+-JavaScript; Mainly using for any client-side activity for a web application, like possible attribute validation or refreshing the page in a spectific interval or provide some dynamic changes in web pages without refreshing the page.
+-NodeJS; It mainley used for accessing or performing any non-blocking operation of any operating system, like creating or executing a shell of script or accessing any hardware-specific information or running any backed job.
+
+3-Running Engine:
+-JavaScript; JavaScript running any engine like Spider monkey (FireFox), JavaScript Core (Safari), V8 (Google Chrome).
+-NodeJS; NodeJS only run in a V8 engine which mainly used by google chrome. And javascript program wich wrriten under this Node JS will be always run in V8 Engine.
+--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+*/
