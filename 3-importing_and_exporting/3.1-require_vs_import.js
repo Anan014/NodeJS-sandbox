5e4954ed5814cdf9886cd5015087174f0d44a876
Anan014 <ananamer95@gmail.com>
2021-11-07 21:42:46 +0200

    ex3.1 done

---
+++ 3-importing_and_exporting/3.1-require_vs_import.js
@@ -0,0 +1,13 @@
+/*
+1) What is the difference between import and require?
+- CommonJS uses: require, module.exports.
+-ES6 uses: import, export.
+-Loading is synchronous(step by step) for require on the other hand import can be asynchronous(without waiting for previous import) so it can perform a little better than require.
+
+2) How can you enable using the import syntax using node js,
+Give 2 node.js environment variables that are not available
+when using the import syntax.
+
+3) Create 3 functions using the export/import syntax.
+Import the file system module using the import syntax.
+*/
