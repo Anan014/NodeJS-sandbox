23cb3113c9792ff7376a05481dde1bb09e419e31
Anan014 <ananamer95@gmail.com>
2021-11-07 23:00:12 +0200

    env-modules

--- env-playground/server.js
+++ env-playground/server.js
@@ -1,3 +1,8 @@
 // server.js
-const port = process.env.PORT;
-console.log(`Your port is ${port}`);
+// console.log(`Your pory is ${process.env.PORT}`); // undefined
+// const dotenv = require('dotenv');
+// dotenv.config();
+// console.log(`Your port is ${process.env.PORT}`); // 8626
+
+const { port } = require('./config');
+console.log(`Your port is ${port}`); //8626
